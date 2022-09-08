6fe33edce156d973feefa6b4be77adcfb4b7c16c
Andreii-Rak <bos9ka@gmail.com>
2022-09-08 22:40:46 +0300

    random

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-#function computes factorial of a given number
+# function computes factorial of a given number
 def factorial(n):
-	result = 1
-	i=1
-	while i<=n:
-		result*=i
-		i+=1
-	return result
+    result = 1
+    i = 1
+    while i <= n:
+        result *= i
+        i += 1
+    return result
 
-#read input from user
+
+# read input from user
 n = int(input('Enter a number: '))
 
-#calculate factorial
+# calculate factorial
 result = factorial(n)
 
-print('Factorial from the input number is:',result)
-
+print('Factorial from the input number is:', result)
 
 # def is_int(str):
 #     try:
@@ -26,10 +26,9 @@ print('Factorial from the input number is:',result)
 # print('Is it an integer:',is_int(a))
 
 if (result % 2) == 0:
-	print("This number is even".format(result))
+    print("This number is even".format(result))
 else:
-	print("This number is odd".format(result))
-
+    print("This number is odd".format(result))
 
 my_array = []
 my_array.append(result)
@@ -38,17 +37,15 @@ my_array.append(result)
 my_array.append(result)
 print(my_array)
 
+
 import random
-random_numbers = random.randint(1,10)
-print("Random number:", random_numbers)
+def rand():
+	return random.randint(1, 10)
 
+my_array.clear()
 
-def custom_randomizer(result, random_numbers):
-	return result * random_numbers
-print(custom_randomizer(result, random_numbers))
-
-my_array[0]= custom_randomizer(result, random_numbers)
-my_array[1]= custom_randomizer(result, random_numbers)
-my_array[2]= custom_randomizer(result, random_numbers)
-my_array[3]= custom_randomizer(result, random_numbers)
+for a in range(4):
+	my_array.append(rand() * result)
 print(my_array)
+
+
